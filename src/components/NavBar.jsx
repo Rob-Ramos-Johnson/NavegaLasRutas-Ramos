09d755ea3244c76e1d79144cb97e2a8cf0342bd0
Roberto <r.a.ramosjohnson@gmail.com>
2025-10-12 02:09:45 -0300

    Avances en NavBar y responsive del carrito

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,46 @@
-import { Link } from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap"; // para links con react-router
+import CartWidget from "./CartWidget";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <Link className="navbar-brand" to="/">CreaTuLanding</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link className="nav-link" to="/">Inicio</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/category/velas">Velas</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/category/incienso">Incienso</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/category/cristales">Cristales</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/category/difusores">Difusores</Link></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Container>
+        {/* Logo */}
+        <LinkContainer to="/">
+          <Navbar.Brand>Tienda Mística</Navbar.Brand>
+        </LinkContainer>
+
+        {/* Toggle (hamburguesa) */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* Menú */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto"> {/* Centrado en pantalla grande */}
+            <LinkContainer to="/">
+              <Nav.Link>Inicio</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/category/velas">
+              <Nav.Link>Velas</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/category/incienso">
+              <Nav.Link>Incienso</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/category/cristales">
+              <Nav.Link>Cristales</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/category/difusores">
+              <Nav.Link>Difusores</Nav.Link>
+            </LinkContainer>
+          </Nav>
+
+          {/* Carrito a la derecha en cualquier pantalla */}
+          <div className="d-flex align-items-center">
+            <CartWidget />
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
